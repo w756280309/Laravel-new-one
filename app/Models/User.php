4a434369d031dd03ac442beb06ebd70baf894fd3
w756280309 <1498373496@qq.com>
2018-08-28 08:05:51 +0000

    fix bug

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Notifications\ResetPassword;
+use Auth;
 
 class User extends Authenticatable
 {
