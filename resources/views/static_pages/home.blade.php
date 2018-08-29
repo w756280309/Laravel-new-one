9f85a721afdb09d659ea04e1a2a812940cead9b4
wangyingjie <1498373496@qq.com>
2018-08-29 18:07:38 +0800

    change

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -23,7 +23,7 @@
         <div class="jumbotron">
             <h1>Hello Laravel</h1>
             <p class="lead">
-                你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的项目主页。
+                你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">小小微博应用</a> 的项目主页。
             </p>
             <p>
                 一切，将从这里开始。
