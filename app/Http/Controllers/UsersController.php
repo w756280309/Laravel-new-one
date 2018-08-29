f682ccf0f04650088375b0063364f9ada61ded18
wangyingjie <1498373496@qq.com>
2018-08-29 18:00:23 +0800

    change

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -102,7 +102,7 @@ class UsersController extends Controller
         $subject = "感谢注册 微博 应用！请确认你的邮箱。";
 
         Mail::send($view, $data, function ($message) use ($to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+            $message->to($to)->subject($subject);
         });
     }
     public function followings(User $user)
